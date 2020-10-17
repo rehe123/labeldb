09b8b416d714ad340aae435139b7a508cbc0cec5
wyh <15655267350@163.com>
2020-10-17 18:05:01 +0800

    带元祖参数的多进程处理

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import os
+import multiprocessing
+import time
+
+
+# 带有参数的任务
+def task(count):
+    for i in range(count):
+        print("任务执行中..")
+        time.sleep(0.2)
+    else:
+        print("任务执行完成")
+
 
 if __name__ == '__main__':
-    pass
+    # 创建子进程
+    # args: 以元组的方式给任务传入参数
+    sub_process = multiprocessing.Process(target=task, args=(5,))
+    sub_process.start()
