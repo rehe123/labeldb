6710487028a0ae7f51941fc4acf9c33910e1c38d
wyh <15655267350@163.com>
2020-11-03 20:48:00 +0800

    20201103

--- utils/web_server/miniWebServer.py
+++ utils/web_server/miniWebServer.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import sys
+from . import framework
 
 
 # 定义web服务器类
@@ -14,16 +15,16 @@ class HttpWebServer(object):
         tcp_server_socket.bind(("", port))
         # 设置监听
         tcp_server_socket.listen(128)
-        # 保存创建成功的服务器套接字
         self.tcp_server_socket = tcp_server_socket
 
-    # 处理客户端的请求
+    # 处理客户的请求
     @staticmethod
-    def handle_client_request(new_socket):
+    def handle_client_quest(new_socket):
         # 代码执行到此，说明连接建立成功
         recv_client_data = new_socket.recv(4096)
         if len(recv_client_data) == 0:
             print("关闭浏览器了")
+            # 关闭服务与客户端的套接字
             new_socket.close()
             return
 
@@ -41,76 +42,106 @@ class HttpWebServer(object):
         if request_path == "/":
             request_path = "/index.html"
 
-        try:
-            # 动态打开指定文件
-            with open("static" + request_path, "rb") as file:
-                # 读取文件数据
-                file_data = file.read()
-        except Exception as e:
-            # 请求资源不存在，返回404数据
-            # 响应行
-            response_line = "HTTP/1.1 404 Not Found\r\n"
-            # 响应头
-            response_header = "Server: PWS1.0\r\n"
-            with open("static/error.html", "rb") as file:
-                file_data = file.read()
-            # 响应体
-            response_body = file_data
+        # 判断是否是动态资源请求
+        if request_path.endswith(".html"):
+            """这里是动态资源请求，把请求信息交给框架处理"""
+            # 字典存储用户的请求信息
+            env = {
+                "request_path": request_path
+            }
 
-            # 拼接响应报文
-            response_data = (response_line + response_header + "\r\n").encode("utf-8") + response_body
+            # 获取处理结果
+            status, headers, response_body = framework.handle_request(env)
+
+            # 使用框架处理的数据拼接响应报文
+            # 响应行
+            response_line = "HTTP/1.1 %s\r\n" % status
+            # 响应头
+            response_header = ""
+            # 遍历头部信息
+            for header in headers:
+                # 拼接多个响应头
+                response_header += "%s: %s\r\n" % header
+            response_data = (response_line +
+                             response_header +
+                             "\r\n" +
+                             response_body).encode("utf-8")
             # 发送数据
             new_socket.send(response_data)
-        else:
-            # 响应行
-            response_line = "HTTP/1.1 200 OK\r\n"
-            # 响应头
-            response_header = "Server: PWS1.0\r\n"
-
-            # 响应体
-            response_body = file_data
-
-            # 拼接响应报文
-            response_data = (response_line + response_header + "\r\n").encode("utf-8") + response_body
-            # 发送数据
-            new_socket.send(response_data)
-        finally:
-            # 关闭服务与客户端的套接字
+            # 关闭socket
             new_socket.close()
 
-    # 启动web服务器进行工作
+        else:
+            """这里是静态资源请求"""
+            try:
+                # 动态打开指定文件
+                with open("static" + request_path, "rb") as file:
+                    # 读取文件数据
+                    file_data = file.read()
+            except Exception as e:
+                # 请求资源不存在，返回404数据
+                # 响应行
+                response_line = "HTTP/1.1 404 Not Found\r\n"
+                # 响应头
+                response_header = "Server: PWS1.0\r\n"
+                with open("static/error.html", "rb") as file:
+                    file_data = file.read()
+                # 响应体
+                response_body = file_data
+
+                # 拼接响应报文
+                response_data = (response_line + response_header + "\r\n").encode("utf-8") + response_body
+                # 发送数据
+                new_socket.send(response_data)
+            else:
+                # 响应行
+                response_line = "HTTP/1.1 200 OK\r\n"
+                # 响应头
+                response_header = "Server: PWS1.0\r\n"
+
+                # 响应体
+                response_body = file_data
+
+                # 拼接响应报文
+                response_data = (response_line + response_header + "\r\n").encode("utf-8") + response_body
+                # 发送数据
+                new_socket.send(response_data)
+            finally:
+                # 关闭服务与客户端的套接字
+                new_socket.close()
+
     def start(self):
         while True:
             # 等待接受客户端的连接请求
             new_socket, ip_port = self.tcp_server_socket.accept()
-            # 当客户端和服务器建立连接程，创建子线程
-            sub_thread = threading.Thread(target=self.handle_client_request, args=(new_socket,))
-            # 设置守护主线程
+            sub_thread = threading.Thread(target=self.handle_client_quest, args=(new_socket,))
+            # 设置守护线程
             sub_thread.setDaemon(True)
-            # 启动子线程执行对应的任务
             sub_thread.start()
 
 
 # 程序入口函数
 def main():
-    print(sys.argv)
-    # 判断命令行参数是否等于2,
+
+    # 获取命令行参数判断长度
     if len(sys.argv) != 2:
-        print("执行命令如下: python3 xxx.py 8000")
+        print("执行命令如下: python3 xxx.py 9000")
         return
 
-    # 判断字符串是否都是数字组成
+    # 判断端口号是否是数字
     if not sys.argv[1].isdigit():
-        print("执行命令如下: python3 xxx.py 8000")
+        print("执行命令如下: python3 xxx.py 9000")
         return
 
-    # 获取终端命令行参数
+    # 需要转成int类型
     port = int(sys.argv[1])
-    # 创建web服务器对象
+
+    # 创建web服务器
     web_server = HttpWebServer(port)
-    # 启动web服务器进行工作
+    # 启动web服务器
     web_server.start()
 
 
 if __name__ == '__main__':
     main()
+
